6c40ebc43c485adaf6e614235cdc4770e167934d
Millie <denisg.co2.1@gmail.com>
2021-01-13 09:47:29 +0200

    a bit of reformatting

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -13,10 +13,6 @@ public class playerMovement : MonoBehaviour
     public float runningSize = 5.5f;
     public float zoomSpeed = 10f;
 
-    [Header("Dashing")]
-    public float dashStrenght = 10f;
-    public float dashCooldown = 4f;
-
     [Header("Gun")]
     public float fireRate = 0.3f;
     public float bulletForce = 20f;
@@ -30,7 +26,6 @@ public class playerMovement : MonoBehaviour
 
     Vector2 movement;
     Vector2 mousePosition;
-    private float lastDash;
     private float lastBullet;
     private float moveSpeed;
 
@@ -39,7 +34,6 @@ public class playerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         lastBullet += Time.deltaTime;
-        lastDash += Time.deltaTime;
 
         mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
 
@@ -49,6 +43,29 @@ public class playerMovement : MonoBehaviour
         gun.rotation = angle;
         gun.position = player.position;
 
+        Running();
+        Shoot();
+    }
+
+    void FixedUpdate()
+    {
+        player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Shoot()
+    {
+
+        if (Input.GetMouseButton(0) && lastBullet > fireRate)
+        {
+            lastBullet = 0;
+            GameObject bullet = Instantiate(bulletPrefab, gunTip.transform.position, gunTip.transform.rotation);
+            Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+            rbBullet.AddForce(gunTip.transform.up * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void Running()
+    {
         if (Input.GetKey(KeyCode.LeftShift))
         {
             moveSpeed = runningSpeed;
@@ -59,23 +76,5 @@ public class playerMovement : MonoBehaviour
             moveSpeed = defaultMoveSpeed;
             cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, normalSize, Time.deltaTime * zoomSpeed);
         }
-
-        if (Input.GetMouseButton(0) && lastBullet > fireRate)
-        {
-            Shoot();
-            lastBullet = 0;
-        }
-    }
-
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, gunTip.transform.position, gunTip.transform.rotation);
-        Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
-        rbBullet.AddForce(gunTip.transform.up * bulletForce, ForceMode2D.Impulse);
-    }
-
-    void FixedUpdate()
-    {
-        player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
